d8ac17682f9b635157e0ddd4919360a570df7006
cgibsonmm <cgibsonmm@gmail.com>
2018-06-20 16:54:20 -0400

    fixed datetime problem migrated

--- db/migrate/20180620204934_create_meetings.rb
+++ db/migrate/20180620204934_create_meetings.rb
@@ -2,8 +2,8 @@ class CreateMeetings < ActiveRecord::Migration[5.1]
   def change
     create_table :meetings do |t|
       t.string :name
-      t.date :start_time
-      t.date :end_time
+      t.datetime :start_time
+      t.datetime :end_time
 
       t.timestamps
     end
